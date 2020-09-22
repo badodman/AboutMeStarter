2083e2b3298eaecc3f3fd75de4430e09f1784727
Brennen <brennen.dodman627@sccdsb.net>
2020-09-22 14:07:36 -0400

    made changes
    put my  name in

--- AboutMe.java
+++ AboutMe.java
@@ -1,24 +1,25 @@
 /*
- * Programmer:
- * Date:
+ * Programmer:BrennenDodman
+ * Date:9/22/2020
  * Purpose:
  */
 
 public class AboutMe {
     public static void main (String [] args) {
         // Declaring the variables
-        // TODO: Write the code the declares the variables
-
+    	String myName = "Brennen";
+    	int myAge = 16;
+    	double myHeightInMetres = 1.92024;
         // Print the values
         System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
-
-        // Change the variables
-        // TODO: Write the code that changes the variables
-
+        // Change the variable 
+        myAge = 32;
+      	myHeightInMetres = 2.58376;
         // Print the new values
-        System.out.println("Some time passes...");
-        // TODO: Write the last two print statements
+        System.out.println("Some time passes...");          
+        System.out.println("I am now " + myAge + " years old.");
+        System.out.println("My height is now " + myHeightInMetres + " m.");
     }
 }
